881326ff74bb0b1552a98579f1b7d7f69311c663
Rosky68 <skybar@volny.cz>
2016-10-17 17:24:46 +0200

    update1

--- Wingit/Form1.Designer.vb
+++ Wingit/Form1.Designer.vb
@@ -23,28 +23,52 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.Button1 = New System.Windows.Forms.Button()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ListView1 = New System.Windows.Forms.ListView()
         Me.SuspendLayout()
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(38, 48)
+        Me.Button1.Location = New System.Drawing.Point(12, 22)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(124, 24)
         Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
+        Me.Button1.Text = "BtnRun"
         Me.Button1.UseVisualStyleBackColor = True
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 239)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(284, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ListView1
+        '
+        Me.ListView1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ListView1.Location = New System.Drawing.Point(0, 90)
+        Me.ListView1.Name = "ListView1"
+        Me.ListView1.Size = New System.Drawing.Size(284, 149)
+        Me.ListView1.TabIndex = 3
+        Me.ListView1.UseCompatibleStateImageBehavior = False
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.ListView1)
+        Me.Controls.Add(Me.StatusStrip1)
         Me.Controls.Add(Me.Button1)
         Me.Name = "Form1"
         Me.Text = "Form1"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents ListView1 As System.Windows.Forms.ListView
 
 End Class
--- Wingit/Form1.vb
+++ Wingit/Form1.vb
@@ -5,7 +5,16 @@
     End Sub
 
     Private Sub aa()
+        AddLV("Button.click")
+    End Sub
 
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        AddLV("Starting ..")
+    End Sub
+
+    Private Sub AddLV(ByVal Mess As String)
+        Dim lvi As ListViewItem = Me.ListView1.Items.Add(Now.ToString)
+        lvi.SubItems.Add(Mess)
     End Sub
 
 End Class
