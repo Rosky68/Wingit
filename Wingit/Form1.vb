7a2388ddde3f5e8c5de45e925c69359c3c4a386d
Rosky68 <skybar@volny.cz>
2016-10-17 17:16:21 +0200

    Import project

---
+++ Wingit/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Call aa()
+    End Sub
+
+    Private Sub aa()
+
+    End Sub
+
+End Class
