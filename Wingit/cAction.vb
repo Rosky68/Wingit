5f1c8587c95e6be503f1d23c0aea85abaf690c72
Rosky68 <skybar@volny.cz>
2016-10-17 18:36:05 +0200

    Init cAction

--- Wingit/cAction.vb
+++ Wingit/cAction.vb
@@ -1,3 +1,14 @@
 ﻿Public Class cAction
+    Private m_ID As Int32
+    Private m_GUID As Guid
+
+    Private Sub New()
+        m_GUID = Guid.NewGuid
+    End Sub
+
+    Public Sub New(ByVal id As Int32)
+        MyBase.New()
+        m_ID = id
+    End Sub
 
 End Class
